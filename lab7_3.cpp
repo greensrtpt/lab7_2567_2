b76e685077b1a689ced83a73a6caf21d08923cea
sireethorn <sireethorn2548.h20p@gmail.com>
2024-12-06 15:25:37 +0700

    lab7_3(Compile).cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char x){
+     if ( x >= 'A' && x <= 'Z'){
+        if (x == 'A'){
+          return 'Z'; 
+            
+        
+        } else {
+          return x-1;
+            
+        }
+
+     }
+return '0';
 }
+int main (){
+    cout << before('A') <<endl;
+    cout << before('B') <<endl;
+    cout << before('P') <<endl;
+    cout << before('T') <<endl;
+    cout << before('Z') <<endl;
+    cout << before('a') <<endl;
+    cout << before('0') <<endl;
+    cout << before('c') <<endl;
+    return 0;
+}   
